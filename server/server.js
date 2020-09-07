87f9e77fed6e624e0027a22302762917e3efc65d
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-09-07 13:13:14 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-const express = require("express");
+// const express = require("express");
+// const api = require("./api/api");
+// const app = express();
+// // app.use(bodyParser);
+// const port = 5000;
+// app.use(express.json());
 
-// const bodyParser = require("body-parser");
-const api = require("./routes/api");
-const app = express();
-// app.use(bodyParser);
-const port = 5000;
-app.use(express.json());
+// app.use("/api", api);
+// app.get("/", (req, res) => {
+//   res.send("hello");
+// });
 
-app.use("/api", api);
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-app.listen(port, () => {
-  console.log(`server is running on port : ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`server is running on port : ${port}`);
+// });
