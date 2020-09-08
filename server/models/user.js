f12efdd4ee41716268c21e5ecc7b266f0777a644
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-09-07 22:16:18 -0400

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const userSchema = new schema({
+const userSchema = new Schema({
   _id: mongoose.Schema.Types.ObjectId,
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
