8cb2531c6210f2e277678332d5ae4c74759f1cd5
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-09-09 23:38:12 -0400

    sidebar update

--- stockportfolio/src/Sidebar.js
+++ stockportfolio/src/Sidebar.js
@@ -5,21 +5,22 @@ import PieChartIcon from '@material-ui/icons/PieChart';
 import DesktopMacIcon from '@material-ui/icons/DesktopMac';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
-
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+import ShowChartIcon from '@material-ui/icons/ShowChart';
 function Sidebar() {
     return (
         <div className="sidebar">
         <div className="my__portfolio">
-        <h6>MY PORTFOLIO</h6>
+        <h5>MY PORTFOLIO</h5>
         <SidebarRow Icon={PieChartIcon} title="Overview"/>
         <SidebarRow Icon={DesktopMacIcon} title="My Holdings"/>
         <SidebarRow Icon ={BarChartIcon}title="My portfolio Analysis"/>
         <SidebarRow Icon ={EmojiObjectsIcon}title="Crowd Insights"/>
         </div>
         <div className="general">
-        <h6>General</h6>
-        <SidebarRow title="My Performance"/>
-        <SidebarRow title="Screener"/>
+        <h5>GENERAL</h5>
+        <SidebarRow Icon={StarHalfIcon} title="My Performance"/>
+        <SidebarRow Icon={ShowChartIcon}title="Screener"/>
         </div>
         </div>
     )
