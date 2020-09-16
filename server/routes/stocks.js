9244bec5d9b7ea5c64fb9a2548ec90f455f424be
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-09-16 01:29:16 -0400

    stock service update

--- server/routes/stocks.js
+++ server/routes/stocks.js
@@ -1,53 +1,52 @@
 const express = require('express');
 const router = express.Router();
-const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-const fetch = require("node-fetch");
 const API_KEY="VBQM4W9ASI86D2H7";
-const https = require('https');
 const axios = require('axios');
 
 router.get("/:symbol",(req,res,next)=>{
     console.log(req.params.symbol);
-    // const xhr = new XMLHttpRequest();
-    // xhr.open("GET",`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${req.params.symbol}&apikey=${API_KEY}`,true);
-   
-    // xhr.onload = function(){    
-    //     console.log(xhr.responseText);
-    //     // let data = JSON.parse(xhr.responseText);
-    //     res.status(200).json(data);
-    // }
-    // xhr.send();
-    // fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${req.params.symbol}&apikey=${API_KEY}`).then(result=>{
-    // let data = JSON.parse(result);
-    // console.log('data:',data);
-    // res.status(200).json(data);
-    // }).catch(error=>{
-    //     console.log(error);
-    // })
-    // https.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${req.params.symbol}&apikey=${API_KEY}`,(result)=>{
-    //     // console.log(JSON.stingify(result));
-    //     // res.status(200).send(JSON.stringify(result));
-    // } )
-
-    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${req.params.symbol}&apikey=${API_KEY}`).then(result=>{
+ 
+    axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${req.params.symbol}&outputsize=full&apikey=${API_KEY}`).then(result=>{
         // console.log(x.data);
         let data = result.data;
         // console.log(data['Time Series (Daily)']);
         let plotStock = [];
-        let temp = result.data['Time Series (Daily)'];
+        plotStock[0]= result.data["Meta Data"]["2. Symbol"];
+        plotStock[1] = [];
+        let count =0;
+        let temp = new Map();
+        // result.data['Time Series (Daily)'];
+       
+        for(let key of Object.keys(result.data['Time Series (Daily)'])){
+            // console.log(key);
+            if(count<365){
+            count++;
+            temp.set(key,result.data['Time Series (Daily)'][key]);
+            }
+            else{
+                break ;
+            }
+        }
+        // console.log('temp');
         let close = '4. close';
-        for(let time of Object.keys(temp)){
+        for(let time of temp.keys()){
+            // console.log(temp.get(time)[close]);
             let dateData =[];
             let timeArray =time.split("-");
+            // console.log(timeArray);
             let date = new Date();
-            date.setFullYear(timeArray[0]);
-            date.setMonth(timeArray[1]);
-            date.setDate(timeArray[2]);
+            date.setFullYear(timeArray[0],timeArray[1]-1,timeArray[2]);
+            // date.setMonth();
+            // date.setDate();
+            // console.log(timeArray[1]);
+            // console.log(date);
             let timeSec = date.getTime();
-            // console.log(timeSec);
-            // console.log(temp[time]['4. close']); 
-            dateData.push(timeSec,parseFloat(temp[time]['4. close']))
-            plotStock.push(dateData);
+            // // console.log(timeSec);
+            // // console.log(temp[time]['4. close']); 
+            // dateData.push(timeSec,parseFloat(temp[time]['4. close']))
+            dateData.push(timeSec,parseFloat(temp.get(time)[close]));
+            // // console.log(temp[time]['4. close']);
+            plotStock[1].push(dateData);
         }
 
 
